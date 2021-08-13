da5e16d685d191a013d6dbf1736ba0ec2db5cce5
Jordan Irwin <antumdeluge@gmail.com>
2021-08-12 23:25:39 -0700

    Don't shut down completely if compatible mob library not found

--- init.lua
+++ init.lua
@@ -3,6 +3,22 @@ skeleton = {}
 skeleton.modname = core.get_current_modname()
 skeleton.modpath = core.get_modpath(skeleton.modname)
 
+function skeleton.log(lvl, msg)
+	if not msg then
+		msg = lvl
+		lvl = nil
+	end
+
+	msg = "[" .. skeleton.modname .. "] " .. msg
+
+	if not lvl then
+		core.log(msg)
+	else
+		core.log(lvl, msg)
+	end
+end
+
+
 -- support mob libraries in order of preference
 local mob_libs = {
 	"mobs",
@@ -19,27 +35,14 @@ end
 
 -- check for compatible library
 if not skeleton.lib then
-	error("a compatible mob library was not found, please install one of the following: "
+	skeleton.log("error", "Could not load. A compatible mob library was not found. Please install one of the following: "
 		.. table.concat(mob_libs, ", "))
+	skeleton = nil
+	do return end
 end
 
 if skeleton.lib == "creatures" then
 	skeleton.lib = "cmer"
-end
-
-function skeleton.log(lvl, msg)
-	if not msg then
-		msg = lvl
-		lvl = nil
-	end
-
-	msg = "[" .. skeleton.modname .. "] " .. msg
-
-	if not lvl then
-		core.log(msg)
-	else
-		core.log(lvl, msg)
-	end
 end
 
 
